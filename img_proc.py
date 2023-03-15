999ad095ae95f13886f4e4df8851a479e2761be4
Javier H <jiherrerap@unal.edu.co>
2023-03-15 13:41:17 -0500

    color palette project - functionality is complete. Forms work. Speed improved siginifcantly changing Kmeans algorithm for MiniKMeans algorithms. Design completed - API working fully correct - api and website give slightly different colors

--- img_proc.py
+++ img_proc.py
@@ -23,3 +23,25 @@ def color_palette(path:str, num_of_colors:int):
     #cluster_colors = kmeans.cluster_centers_[labels]
     #ax[1].scatter(normalized_colors[:,0], normalized_colors[:,1], color=cluster_colors)
     #plt.show()
+
+def api_color(image_file, num_color=3):
+    img = Image.open(image_file)
+    np_img = np.array(img)
+    resolution = np_img.shape
+    np_img = np_img.reshape(np_img.shape[0] * np_img.shape[1], np_img.shape[2])
+    normalized_colors = np_img.astype('float32') / 255
+    kmeans = MiniBatchKMeans(n_clusters=num_color)
+    kmeans.fit(normalized_colors)
+    labels = kmeans.labels_
+    count = np.bincount(labels)
+    cluster_centers = [tuple(i) for i in kmeans.cluster_centers_ * 255]
+    color_counts = list(zip(cluster_centers, count))
+    color_counts.sort(reverse=True, key=lambda x: x[1])
+    rounded_colors = [(tuple(round(num, 1) for num in color), px_num) for color, px_num in color_counts]
+    num_of_px = len(normalized_colors)
+    response_dict = [{f'color{ind}': f'{data[0]}', 'pxs': f'{data[1]}'} for ind, data in enumerate(rounded_colors)]
+    response_dict = {'extracted': response_dict,
+                     'resolution': f'{resolution}',
+                     'total_pxls': str(num_of_px),
+                     'cluester_centers': str(num_color)}
+    return response_dict
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for, jsonify, request
 import os
 from forms import uploadForm
 import werkzeug
-from img_proc import color_palette
+from img_proc import color_palette, api_color
 from delete_image import delete_image
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './static/images'
@@ -14,7 +15,6 @@ def rounding(num: float):
     return round(num, 2)
 
 
-@app.route('/test', methods=['POST', 'GET'])
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/home', methods=['POST', 'GET'])
 def test():
@@ -32,16 +32,24 @@ def test():
         f.save(img_path)
         colors, tot_pxs = color_palette(img_path, form.colors.data)
         return render_template('index3.html', img_path=img_path, form=form, colors=colors, tot_pxs=tot_pxs)
-    elif form.is_submitted() and not form.validate():
-        print('errors in form', form.file.errors)
-        flash(f'ONLY IMAGE-TYPE FILES')
+    elif form.is_submitted():
+        if 'This field is required.' in form.file.errors:
+            print('errors in form', form.file.errors)
+            flash(f'Select an image')
+        else:
+            print('errors in form', form.file.errors)
+            flash(f'We only accept images')
         return redirect(url_for('test'))
 
-    return render_template('index3.html', img_path=img_path, form=form, colors=colors, tot_pxs=tot_pxs)
+    return render_template('index3.html', img_path=img_path, form=form, colors=colors, tot_pxs=tot_pxs, year=datetime.now().year)
+
+@app.route('/api', methods = ['GET', 'POST'])
+def api_response():
+    if request.method == 'POST':
+        data_recived = request.files['image']
+    return jsonify(api_color(data_recived.stream, num_color=int(request.values.get('num_colors'))))
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app.run(debug=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
